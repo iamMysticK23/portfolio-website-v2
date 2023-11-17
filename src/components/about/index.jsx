5ec913ebddaa492db431d400d54453c57f5b76df
Kenai Epps <k.epps23@gmail.com>
2023-11-17 11:11:40 -0800

    fixed mobile image for personal info

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -5,7 +5,7 @@ import Experience from "./Experience";
 import PersonalInfo from "./PersonalInfo";
 import Skills from "./Skills";
 import cv from "../../assets/img/KenaiEpps_FE_Dev2023.pdf";
-import heroImgMobile from "../../assets/img/hero/img-mobile.jpg";
+import heroImgMobile from "../../assets/img/hero/hero-img-mobile.png";
 
 const index = () => {
   return (
